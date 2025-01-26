e280b8e90a783eb8dcfafc17e68cbe5f084f590c
Yash Pratap Singh <yashpratapsingh577@gmail.com>
2025-01-26 16:13:52 +0530

    update: refactored Add and Edit TimerModals into single component

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import { Plus, Clock } from "lucide-react";
 import { TimerList } from "./components/TimerList";
-import { AddTimerModal } from "./components/AddTimerModal";
 import { Toaster } from "sonner";
 import { PrimaryButton } from "./components/PrimaryButton";
+import { TimerModal } from "./components/TimerModal";
 
 function Home() {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -28,7 +28,7 @@ function Home() {
 
         <TimerList />
 
-        <AddTimerModal
+        <TimerModal
           isOpen={isModalOpen}
           onClose={() => setIsModalOpen(false)}
         />
